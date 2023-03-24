0e75f96fdcecb4c81fa98e8c2bb7e24b0a0a02f8
DanielNasc <danielnasc15987@gmail.com>
2023-03-24 16:58:01 -0300

    Double_to_Binary::Added ->  comments, special cases for NAN, INF and -INF

---
+++ asm/mips/basic/seila.asm
@@ -0,0 +1,18 @@
+.data
+    	um:	.double	1.0
+    	dois:	.double	2.0
+.text
+	.globl divv
+		l.d	$f0,	um
+		l.d	$f2,	um
+		l.d	$f4, 	dois
+	divv: 					# loop
+		div.d	$f0,	$f0,	$f4 	# divisão: eps = eps / 2
+		add.d	$f6,	$f2,	$f0 	# soma: soma = 1 + eps
+		c.eq.d	$f6, 	$f2		# checa: soma == 1
+		bc1f divv
+	mul.d	$f2,	$f0,	$f4		# last number x that satisfies x + 1 = 1: 
+	add.d	$f10,	$f2,	$f0		# soma: 1 + eps
+	
+	
+	
